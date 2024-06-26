3c986cb1b44bea45c8628a5385ec63ab91b71300
OishiInary <168375833+OishiInary@users.noreply.github.com>
2024-06-26 13:45:50 +0000

    6/26

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+  get 'books/new'
+  get 'books/index'
+  get 'books/show'
+  get 'books/edit'
+  root 'welcome#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
