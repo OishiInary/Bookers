5c35825e74cdbed8e3d913a0563ac4bef2a98c75
OishiInary <168375833+OishiInary@users.noreply.github.com>
2024-07-02 08:15:34 +0000

    7/2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,14 +1,45 @@
 class BooksController < ApplicationController
   def new
+    @book = Book.new(book_params)
   end
 
   def index
-    @lists = book.all
+    @books = Book.all
+    @book = Book.new
   end
-
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = "Book was successfully created."
+     redirect_to book_path(@book.id)
+    else
+      flash[:alert] = "Book was not successfully created."
+     @books = Book.all
+     render :index
+    end
+  end
+  
   def show
+      @book = Book.find(params[:id])
   end
 
   def edit
+      @book = Book.find(params[:id])
   end
-end
+  
+  def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    redirect_to book_path(@book.id)
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to '/books'
+  end  
+private
+ def book_params
+  params.require(:book).permit(:title,:body)
+ end
+end 
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   get 'welcome/index'
-  get 'books/new'
-  get 'books/index'
-  get 'books/show'
-  get 'books/edit'
-  root 'welcome#index'
+  resources :books
+  #et 'books/:id/edit' => 'books#edit', as: 'edit_book'
+  #patch 'books/:id' => 'books#update', as: 'update_book'
+  #delete 'books/:id' => 'books#destroy', as: 'destroy_book'
+  root'welcome#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
