18d1ac5dfb88d31ca881408065bec39d140903d2
OishiInary <168375833+OishiInary@users.noreply.github.com>
2024-07-03 00:10:28 +0000

    課題：Bookers 完了1

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -13,7 +13,7 @@ class BooksController < ApplicationController
     flash[:notice] = "Book was successfully created."
      redirect_to book_path(@book.id)
     else
-      flash[:alert] = "Book was not successfully created."
+      flash[:alert] = "error!! Book was not successfully updated."
      @books = Book.all
      render :index
     end
@@ -24,6 +24,7 @@ class BooksController < ApplicationController
   end
 
   def edit
+      
       @book = Book.find(params[:id])
   end
   
